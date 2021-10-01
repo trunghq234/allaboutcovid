e387b3abf6885dc5dbf9b8f01b729a8d102fd103
Trung Huỳnh <19520317@ms.uit.edu.vn>
2021-10-02 00:24:16 +0700

    Fix meta tags

--- src/App.js
+++ src/App.js
@@ -26,70 +26,38 @@ function App() {
   }, []);
 
   return (
-    <>
-      <MetaTags>
-        <title>All about COVID</title>
-        <meta
-          name="description"
-          content="A website dedicated to providing global information about the COVID-19 pandemic."
-        />
-        <meta property="og:type" content="website" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <meta property="og:title" content="All about COVID" />
-        <meta
-          property="og:image"
-          content={
-            'https://drive.google.com/uc?export=view&id=1nlAiun06kUwjq6YYGpU2jJ5Wk9UtMSCZ'
-          }
-        />
-        <script
-          async
-          src={`https://www.googletagmanager.com/gtag/js?id=${trackingId}`}
-        />
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `
-              window.dataLayer = window.dataLayer || [];
-              function gtag(){dataLayer.push(arguments);}
-              gtag('js', new Date());
-              gtag('config', '${trackingId}', { page_path: window.location.pathname });
-            `,
-          }}
-        />
-      </MetaTags>
-      <Router>
-        <Layout className="mainLayout">
-          <Header>
-            <AppHeader />
-          </Header>
-          <Switch>
-            <Route exact path="/">
-              <Content>
-                <AppWorld />
-              </Content>
-            </Route>
-            <Route exact path="/vietnam">
-              <Content>
-                <VietNam />
-              </Content>
-            </Route>
-            <Route exact path="/vaccine">
-              <Content>
-                <Vaccine />
-              </Content>
-            </Route>
-            <Route exact path="/news">
-              <Content>
-                <AppNews />
-              </Content>
-            </Route>
-          </Switch>
-          <Footer>
-            <AppFooter />
-          </Footer>
-        </Layout>
-      </Router>
-    </>
+    <Router>
+      <Layout className="mainLayout">
+        <Header>
+          <AppHeader />
+        </Header>
+        <Switch>
+          <Route exact path="/">
+            <Content>
+              <AppWorld />
+            </Content>
+          </Route>
+          <Route exact path="/vietnam">
+            <Content>
+              <VietNam />
+            </Content>
+          </Route>
+          <Route exact path="/vaccine">
+            <Content>
+              <Vaccine />
+            </Content>
+          </Route>
+          <Route exact path="/news">
+            <Content>
+              <AppNews />
+            </Content>
+          </Route>
+        </Switch>
+        <Footer>
+          <AppFooter />
+        </Footer>
+      </Layout>
+    </Router>
   );
 }
 
